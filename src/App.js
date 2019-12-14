38362491d12123225348ad685e4243e94983452d
unknown <dacqcastro@gmail.com>
2019-12-14 09:14:48 -0700

    added workable server link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import jwtDecode from "jwt-decode";
 
+import axios from "axios";
+
 // Components import
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
@@ -11,6 +13,8 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import AuthRoute from "./components/AuthRoute";
 import PrivateRoute from "./components/PrivateRoute";
+
+axios.defaults.baseURL = "https://socialappp.herokuapp.com/";
 
 const token = localStorage.getItem("auth-token");
 
